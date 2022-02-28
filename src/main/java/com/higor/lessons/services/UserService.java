2f1543aa262435cba3f67a1e2bde74efa1ba557e
Higor Rafael Moura RabÊlo <86304823+higorrabelo@users.noreply.github.com>
2022-02-28 10:41:16 -0300

    Exception handling delete

--- src/main/java/com/higor/lessons/services/UserService.java
+++ src/main/java/com/higor/lessons/services/UserService.java
@@ -2,10 +2,15 @@ package com.higor.lessons.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
 import com.higor.lessons.entities.User;
 import com.higor.lessons.repositories.UserRepository;
+import com.higor.lessons.services.exceptions.DatabaseException;
 import com.higor.lessons.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);			
+		}
+		catch(DataIntegrityViolationException a) {
+			throw new DatabaseException(a.getMessage());	
+		}
 	}
 	
 	public User update(Long id, User obj) {
